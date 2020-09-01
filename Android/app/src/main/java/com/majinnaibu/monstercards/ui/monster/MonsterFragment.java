e526f8ce4b16ddc8db8ac544f720a0e96f84b0f7
Tom Hicks <headhunter3@gmail.com>
2020-09-01 00:09:58 -0700

    Fixes deprecation warnings about ViewModelProviders.of(...).

--- Android/app/src/main/java/com/majinnaibu/monstercards/ui/monster/MonsterFragment.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/monster/MonsterFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.majinnaibu.monstercards.R;
 
@@ -20,8 +20,7 @@ public class MonsterFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        monsterViewModel =
-                ViewModelProviders.of(this).get(MonsterViewModel.class);
+        monsterViewModel = new ViewModelProvider(this).get(MonsterViewModel.class);
         View root = inflater.inflate(R.layout.fragment_monster, container, false);
         final TextView textView = root.findViewById(R.id.text_monster);
         monsterViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
