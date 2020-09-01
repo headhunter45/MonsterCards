9eccc93b1e9afabbf59dbf37dac04fc3075194ec
Tom Hicks <headhunter3@gmail.com>
2020-09-01 00:30:12 -0700

    Adds monster name to monster cards.

---
+++ Android/app/src/main/java/com/majinnaibu/monstercards/models/Monster.java
@@ -0,0 +1,13 @@
+package com.majinnaibu.monstercards.models;
+
+public class Monster {
+
+    private String mName;
+    public String getName() {
+        return mName;
+    }
+    public void setName(String value) {
+        mName = value;
+    }
+
+}
