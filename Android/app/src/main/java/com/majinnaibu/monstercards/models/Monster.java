920344b5fd4d961460d03e529371760899bd0980
Tom Hicks <headhunter3@gmail.com>
2020-09-01 00:44:16 -0700

    Adds monster meta (size, type, subtype/tag, and alignment) to monster cards.

--- Android/app/src/main/java/com/majinnaibu/monstercards/models/Monster.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/models/Monster.java
@@ -1,5 +1,10 @@
 package com.majinnaibu.monstercards.models;
 
+import com.majinnaibu.monstercards.helpers.StringHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Monster {
 
     private String mName;
@@ -8,6 +13,78 @@ public class Monster {
     }
     public void setName(String value) {
         mName = value;
+    }
+
+    private String mSize;
+    public String getSize() {
+        return mSize;
+    }
+    public void setSize(String value) {
+        mSize = value;
+    }
+
+    private String mType;
+    public String getType() {
+        return mType;
+    }
+    public void setType(String value) {
+        mType = value;
+    }
+
+    private String mTag;
+    public String getTag() {
+        return mTag;
+    }
+    public void setTag(String value) {
+        mTag = value;
+    }
+
+    private String mAlignment;
+    public String getAlignment() {
+        return mAlignment;
+    }
+    public void setAlignment(String value) {
+        mAlignment = value;
+    }
+
+    public String getMeta() {
+        StringBuilder sb = new StringBuilder();
+        boolean isFirstOutput = true;
+        String size = getSize();
+        if (!StringHelper.isNullOrEmpty(size)) {
+            sb.append(size);
+            isFirstOutput = false;
+        }
+
+        String type = getType();
+        if (!StringHelper.isNullOrEmpty(type)) {
+            if (!isFirstOutput) {
+                sb.append(" ");
+            }
+            sb.append(type);
+            isFirstOutput = false;
+        }
+
+        String tag = getTag();
+        if (!StringHelper.isNullOrEmpty(tag)) {
+            if (!isFirstOutput) {
+                sb.append(" ");
+            }
+            sb.append("(");
+            sb.append(tag);
+            sb.append(")");
+            isFirstOutput = false;
+        }
+
+        String alignment = getAlignment();
+        if (!StringHelper.isNullOrEmpty(alignment)) {
+            if (!isFirstOutput) {
+                sb.append(", ");
+            }
+            sb.append(alignment);
+        }
+
+        return sb.toString();
     }
 
 }
--- Android/app/src/main/java/com/majinnaibu/monstercards/ui/monster/MonsterFragment.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/monster/MonsterFragment.java
@@ -15,6 +15,7 @@ import androidx.lifecycle.ViewModelProvider;
 import com.majinnaibu.monstercards.R;
 import com.majinnaibu.monstercards.models.Monster;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class MonsterFragment extends Fragment {
 
     private MonsterViewModel monsterViewModel;
@@ -24,7 +25,13 @@ public class MonsterFragment extends Fragment {
 
         // TODO: remove this block make the monster ID a parameter to the view and get the monster from saved data (sqlite)
         Monster monster = new Monster();
+        // Name
         monster.setName("Pixie");
+        // Meta
+        monster.setSize("tiny");
+        monster.setType("fey");
+        monster.setTag("");
+        monster.setAlignment("neutral good");
         // END remove block
         monsterViewModel = new ViewModelProvider(this).get(MonsterViewModel.class);
         View root = inflater.inflate(R.layout.fragment_monster, container, false);
@@ -35,6 +42,14 @@ public class MonsterFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String name) {
                 monsterName.setText(name);
+            }
+        });
+
+        final TextView monsterMeta = root.findViewById(R.id.meta);
+        monsterViewModel.getMeta().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                monsterMeta.setText(s);
             }
         });
 
