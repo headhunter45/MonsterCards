74b0c6695f083aa5f950fba7acaae61aa34d5dd0
Tom Hicks <headhunter3@gmail.com>
2020-08-31 22:38:29 -0700

    Adds AppCenter config.

--- Android/app/src/main/java/com/majinnaibu/monstercards/MainActivity.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/MainActivity.java
@@ -9,12 +9,23 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (BuildConfig.APPCENTER_SECRET != null && !"".equals(BuildConfig.APPCENTER_SECRET)) {
+            AppCenter.start(
+                    getApplication(),
+                    BuildConfig.APPCENTER_SECRET,
+                    Analytics.class,
+                    Crashes.class
+            );
+        }
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
