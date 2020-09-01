3b41743c29c08d761625802597bd3e257f3c9ea0
Tom Hicks <headhunter3@gmail.com>
2020-09-01 00:09:58 -0700

    Fixes deprecation warnings about ViewModelProviders.of(...).

--- app/src/main/java/com/majinnaibu/monstercards/ui/search/SearchFragment.java
+++ app/src/main/java/com/majinnaibu/monstercards/ui/search/SearchFragment.java
@@ -11,7 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
@@ -23,8 +23,7 @@ public class SearchFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        searchViewModel =
-                ViewModelProviders.of(this).get(SearchViewModel.class);
+        searchViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
         View root = inflater.inflate(R.layout.fragment_search, container, false);
         final TextView textView = root.findViewById(R.id.search_query);
         searchViewModel.getSearchQuery().observe(getViewLifecycleOwner(), new Observer<String>() {
