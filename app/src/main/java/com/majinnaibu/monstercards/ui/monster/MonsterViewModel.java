c461ca2188d5efe860500080cf26cd1afa78e42b
Tom Hicks <headhunter3@gmail.com>
2020-09-01 21:39:36 -0700

    Adds ability scores to monster card.

--- app/src/main/java/com/majinnaibu/monstercards/ui/monster/MonsterViewModel.java
+++ app/src/main/java/com/majinnaibu/monstercards/ui/monster/MonsterViewModel.java
@@ -20,6 +20,18 @@ public class MonsterViewModel extends ViewModel {
         mHitPoints.setValue("");
         mSpeed = new MutableLiveData<>();
         mSpeed.setValue("");
+        mStrength = new MutableLiveData<>();
+        mStrength.setValue("");
+        mDexterity = new MutableLiveData<>();
+        mDexterity.setValue("");
+        mConstitution = new MutableLiveData<>();
+        mConstitution.setValue("");
+        mIntelligence = new MutableLiveData<>();
+        mIntelligence.setValue("");
+        mWisdom = new MutableLiveData<>();
+        mWisdom.setValue("");
+        mCharisma = new MutableLiveData<>();
+        mCharisma.setValue("");
     }
 
     private MutableLiveData<String> mName;
@@ -42,6 +54,30 @@ public class MonsterViewModel extends ViewModel {
     public LiveData<String> getSpeed() {
         return mSpeed;
     }
+    private MutableLiveData<String> mStrength;
+    public LiveData<String> getStrength() {
+        return mStrength;
+    }
+    private MutableLiveData<String> mDexterity;
+    public LiveData<String> getDexterity() {
+        return mDexterity;
+    }
+    private MutableLiveData<String> mConstitution;
+    public LiveData<String> getConstitution() {
+        return mConstitution;
+    }
+    private MutableLiveData<String> mIntelligence;
+    public LiveData<String> getIntelligence() {
+        return mIntelligence;
+    }
+    private MutableLiveData<String> mWisdom;
+    public LiveData<String> getWisdom() {
+        return mWisdom;
+    }
+    private MutableLiveData<String> mCharisma;
+    public LiveData<String> getCharisma() {
+        return mCharisma;
+    }
 
     private Monster mMonster;
     public void setMonster(Monster monster) {
@@ -51,5 +87,11 @@ public class MonsterViewModel extends ViewModel {
         mArmorClass.setValue(mMonster.getArmorClass());
         mHitPoints.setValue(mMonster.getHitPoints());
         mSpeed.setValue(mMonster.getSpeedText());
+        mStrength.setValue(monster.getStrengthDescription());
+        mDexterity.setValue(monster.getDexterityDescription());
+        mConstitution.setValue(monster.getConstitutionDescription());
+        mIntelligence.setValue(monster.getIntelligenceDescription());
+        mWisdom.setValue(monster.getWisdomDescription());
+        mCharisma.setValue(monster.getCharismaDescription());
     }
 }
