67375292a592b6a11f26e24e97f8770cda12f3c3
Tom Hicks <headhunter3@gmail.com>
2020-09-01 23:42:09 -0700

    Adds abilities to monster cards. Adds CommonMark dependency and CommonMarkHelper to render it to html.

--- Android/app/src/main/java/com/majinnaibu/monstercards/helpers/CommonMarkHelper.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/helpers/CommonMarkHelper.java
@@ -4,9 +4,20 @@ import org.commonmark.node.Document;
 import org.commonmark.node.Node;
 import org.commonmark.node.Paragraph;
 import org.commonmark.parser.Parser;
+import org.commonmark.renderer.NodeRenderer;
+import org.commonmark.renderer.html.HtmlNodeRendererContext;
+import org.commonmark.renderer.html.HtmlNodeRendererFactory;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 public final class CommonMarkHelper {
+    private static final class MyNodeRendererFactory implements HtmlNodeRendererFactory {
+
+        @Override
+        public NodeRenderer create(HtmlNodeRendererContext context) {
+            return null;
+        }
+    }
+
     public static String toHtml(String rawCommonMark) {
         Parser parser = Parser.builder().build();
         Node document = parser.parse(rawCommonMark);
@@ -15,7 +26,7 @@ public final class CommonMarkHelper {
         if (parent1 == parent2 && parent1 instanceof Paragraph) {
             document = new Document();
             Node child = parent1.getFirstChild();
-            while (child != null) {
+            while(child != null) {
                 Node nextChild = child.getNext();
                 document.appendChild(child);
                 child = nextChild;//child.getNext();
