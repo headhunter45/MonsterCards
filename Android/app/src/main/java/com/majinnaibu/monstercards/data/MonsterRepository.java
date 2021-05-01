43475450f9501abae44d3736de3b64b3e37b6cab
Tom Hicks <headhunter3@gmail.com>
2021-04-30 23:53:38 -0700

    Adds MonsterRepository to manage access to the RoomDB store.

---
+++ Android/app/src/main/java/com/majinnaibu/monstercards/data/MonsterRepository.java
@@ -0,0 +1,59 @@
+package com.majinnaibu.monstercards.data;
+
+import androidx.annotation.NonNull;
+
+import com.majinnaibu.monstercards.AppDatabase;
+import com.majinnaibu.monstercards.models.Monster;
+
+import java.util.List;
+import java.util.UUID;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MonsterRepository {
+
+    private AppDatabase m_db;
+
+    public MonsterRepository(@NonNull AppDatabase db) {
+        m_db = db;
+    }
+
+    public Flowable<List<Monster>> getMonsters() {
+
+        return m_db.monsterDAO()
+                .getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public Flowable<Monster> getMonster(UUID monsterId) {
+        return m_db.monsterDAO()
+                .loadAllByIds(new String[]{monsterId.toString()})
+                .map(
+                        monsters -> {
+                            if (monsters.size() > 0) {
+                                return monsters.get(0);
+                            } else {
+                                return null;
+                            }
+                        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public Completable addMonster(Monster monster) {
+        Completable result = m_db.monsterDAO().insertAll(monster);
+        result.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        return result;
+    }
+
+    public Completable deleteMonster(Monster monster) {
+        Completable result = m_db.monsterDAO().delete(monster);
+        result.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        return result;
+    }
+
+}
