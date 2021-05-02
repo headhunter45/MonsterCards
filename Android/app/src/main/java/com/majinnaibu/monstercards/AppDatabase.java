78e7f3ffe46327edc937c716b340258c1ea4c983
Tom Hicks <headhunter3@gmail.com>
2021-05-01 22:34:25 -0700

    Removes unused type converter.

--- Android/app/src/main/java/com/majinnaibu/monstercards/AppDatabase.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/AppDatabase.java
@@ -8,7 +8,6 @@ import com.majinnaibu.monstercards.data.MonsterDAO;
 import com.majinnaibu.monstercards.data.converters.ArmorTypeConverter;
 import com.majinnaibu.monstercards.data.converters.ChallengeRatingConverter;
 import com.majinnaibu.monstercards.data.converters.SetOfLanguageConverter;
-import com.majinnaibu.monstercards.data.converters.SetOfSavingThrowConverter;
 import com.majinnaibu.monstercards.data.converters.SetOfSkillConverter;
 import com.majinnaibu.monstercards.data.converters.SetOfStringConverter;
 import com.majinnaibu.monstercards.data.converters.SetOfTraitConverter;
@@ -22,7 +21,6 @@ import com.majinnaibu.monstercards.models.MonsterFTS;
         ArmorTypeConverter.class,
         ChallengeRatingConverter.class,
         SetOfLanguageConverter.class,
-        SetOfSavingThrowConverter.class,
         SetOfSkillConverter.class,
         SetOfStringConverter.class,
         SetOfTraitConverter.class,
