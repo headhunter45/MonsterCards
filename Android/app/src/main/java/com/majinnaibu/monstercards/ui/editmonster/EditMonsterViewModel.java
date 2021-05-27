d52102d43004715f273fc286bc941f0fc3398c5e
Tom Hicks <headhunter3@gmail.com>
2021-05-27 03:24:33 -0700

    Makes EditMonsterFragment load the monster based with the id passed in and show it's name in the title.

--- Android/app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditMonsterViewModel.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditMonsterViewModel.java
@@ -1,7 +1,39 @@
 package com.majinnaibu.monstercards.ui.editmonster;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.majinnaibu.monstercards.models.Monster;
+
+import java.util.UUID;
+
 public class EditMonsterViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> mName;
+    private MutableLiveData<UUID> mMonsterId;
+
+    public EditMonsterViewModel() {
+
+        mName = new MutableLiveData<>();
+        mName.setValue("");
+
+        mMonsterId = new MutableLiveData<>();
+        mMonsterId.setValue(UUID.randomUUID());
+    }
+
+    public void copyFromMonster(Monster monster) {
+        // TODO: copy from monster to other fields
+        mMonsterId.setValue(monster.id);
+        mName.setValue(monster.name);
+    }
+
+    public LiveData<String> getName() {
+        return mName;
+    }
+
+    public void setName(@NonNull String name) {
+        mName.setValue(name);
+    }
+
 }
