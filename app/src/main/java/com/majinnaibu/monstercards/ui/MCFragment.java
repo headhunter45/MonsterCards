dc631839cf27c2452a51201ee00330c6d7fb2f87
Tom Hicks <headhunter3@gmail.com>
2021-05-27 03:18:17 -0700

    Adds helper method to MCFragment to get the AppCompatActivity for the fragment.

--- app/src/main/java/com/majinnaibu/monstercards/ui/MCFragment.java
+++ app/src/main/java/com/majinnaibu/monstercards/ui/MCFragment.java
@@ -1,5 +1,6 @@
 package com.majinnaibu.monstercards.ui;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.majinnaibu.monstercards.MonsterCardsApplication;
@@ -12,5 +13,9 @@ public class MCFragment extends Fragment {
 
     protected MonsterRepository getMonsterRepository() {
         return this.getApplication().getMonsterRepository();
+    }
+
+    public AppCompatActivity requireAppCompatActivity() {
+        return (AppCompatActivity) requireActivity();
     }
 }
