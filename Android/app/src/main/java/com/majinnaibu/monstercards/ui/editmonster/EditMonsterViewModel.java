73c81f895629e199dc29969f373affe96e974f5d
Tom Hicks <headhunter3@gmail.com>
2021-05-27 01:15:46 -0700

    Replaces EditMonsterFragment with a basic ConstraintLayout.

---
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditMonsterViewModel.java
@@ -0,0 +1,7 @@
+package com.majinnaibu.monstercards.ui.editmonster;
+
+import androidx.lifecycle.ViewModel;
+
+public class EditMonsterViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
