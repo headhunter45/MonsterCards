acef784c6d95fd99f3927c09accb157e59a30368
Tom Hicks <headhunter3@gmail.com>
2021-05-27 03:24:33 -0700

    Makes EditMonsterFragment load the monster based with the id passed in and show it's name in the title.

--- app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditMonsterFragment.java
+++ app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditMonsterFragment.java
@@ -8,13 +8,20 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.majinnaibu.monstercards.R;
+import com.majinnaibu.monstercards.data.MonsterRepository;
+import com.majinnaibu.monstercards.models.Monster;
+import com.majinnaibu.monstercards.ui.MCFragment;
+import com.majinnaibu.monstercards.ui.monster.MonsterDetailFragmentArgs;
 import com.majinnaibu.monstercards.utils.Logger;
 
-public class EditMonsterFragment extends Fragment {
+import java.util.UUID;
+
+import io.reactivex.rxjava3.observers.DisposableSingleObserver;
+
+public class EditMonsterFragment extends MCFragment {
 
     private EditMonsterViewModel mViewModel;
     private ViewHolder mHolder;
@@ -26,13 +33,35 @@ public class EditMonsterFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        // TODO: Get our monster from the repo based on the id passed in.
-        // TODO: Show a loading spinner until we have the monster loaded.
-        // TODO: If there is an error loading the monster show the error.
+
+        MonsterRepository repository = getMonsterRepository();
+        Bundle arguments = getArguments();
+        assert arguments != null;
+        UUID monsterId = UUID.fromString(MonsterDetailFragmentArgs.fromBundle(arguments).getMonsterId());
 
         View root = inflater.inflate(R.layout.fragment_edit_monster, container, false);
-
         mHolder = new ViewHolder(root);
+        requireAppCompatActivity().getSupportActionBar().setTitle(getString(R.string.title_edit_monster, getString(R.string.default_monster_name)));
+
+        // TODO: Show a loading spinner until we have the monster loaded.
+        repository.getMonster(monsterId).toObservable()
+                .firstOrError()
+                .subscribe(new DisposableSingleObserver<Monster>() {
+                    @Override
+                    public void onSuccess(@io.reactivex.rxjava3.annotations.NonNull Monster monster) {
+                        Logger.logDebug(String.format("Monster loaded: %s", monster.name));
+                        mViewModel.copyFromMonster(monster);
+                        requireAppCompatActivity().getSupportActionBar().setTitle(getString(R.string.title_edit_monster, monster.name));
+                        dispose();
+                    }
+
+                    @Override
+                    public void onError(@io.reactivex.rxjava3.annotations.NonNull Throwable e) {
+                        // TODO: Show an error state.
+                        Logger.logError(e);
+                        dispose();
+                    }
+                });
 
         mHolder.basicInfoButton.setOnClickListener(v -> {
             // TODO: Navigate to the EditBasicInfo fragment
