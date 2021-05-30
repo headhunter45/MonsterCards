ab5a3c7c67b904cf748959a9771dde1426b8b71f
Tom Hicks <headhunter3@gmail.com>
2021-05-30 00:10:44 -0700

    Adds Edit Speed screen to the monster editor.

--- Android/app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditSpeedFragment.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditSpeedFragment.java
@@ -4,52 +4,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavBackStackEntry;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.majinnaibu.monstercards.R;
-import com.majinnaibu.monstercards.ui.components.Stepper;
-import com.majinnaibu.monstercards.ui.shared.MCFragment;
 import com.majinnaibu.monstercards.utils.TextChangedListener;
 
-public class EditSpeedFragment extends MCFragment {
+public class EditSpeedFragment extends Fragment {
     private EditMonsterViewModel mViewModel;
     private ViewHolder mHolder;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
         NavBackStackEntry backStackEntry = navController.getBackStackEntry(R.id.edit_monster_navigation);
         mViewModel = new ViewModelProvider(backStackEntry).get(EditMonsterViewModel.class);
+
+        // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_edit_speed, container, false);
+
         mHolder = new ViewHolder(root);
 
-        mHolder.baseSpeed.setOnValueChangeListener((newValue, oldValue) -> mViewModel.setWalkSpeed(newValue));
-        mHolder.baseSpeed.setOnFormatValueCallback(value -> String.format(getString(R.string.format_distance_in_feet), value));
-        mViewModel.getWalkSpeed().observe(getViewLifecycleOwner(), value -> mHolder.baseSpeed.setValue(value));
+        mViewModel.getWalkSpeed().observe(getViewLifecycleOwner(), value -> {
+            mHolder.baseSpeed.setText(String.format(getString(R.string.format_distance_in_feet), value));
+        });
+        mHolder.incrementBaseSpeed.setOnClickListener(v -> mViewModel.incrementWalkSpeed());
+        mHolder.decrementBaseSpeed.setOnClickListener(v -> mViewModel.decrementWalkSpeed());
 
-        mHolder.burrowSpeed.setOnValueChangeListener((newValue, oldValue) -> mViewModel.setBurrowSpeed(newValue));
-        mHolder.burrowSpeed.setOnFormatValueCallback(value -> String.format(getString(R.string.format_distance_in_feet), value));
-        mViewModel.getBurrowSpeed().observe(getViewLifecycleOwner(), value -> mHolder.burrowSpeed.setValue(value));
+        mViewModel.getBurrowSpeed().observe(getViewLifecycleOwner(), value -> {
+            mHolder.burrowSpeed.setText(String.format(getString(R.string.format_distance_in_feet), value));
+        });
+        mHolder.incrementBurrowSpeed.setOnClickListener(v -> mViewModel.incrementBurrowSpeed());
+        mHolder.decrementBurrowSpeed.setOnClickListener(v -> mViewModel.decrementBurrowSpeed());
 
-        mHolder.climbSpeed.setOnValueChangeListener((newValue, oldValue) -> mViewModel.setClimbSpeed(newValue));
-        mHolder.climbSpeed.setOnFormatValueCallback(value -> String.format(getString(R.string.format_distance_in_feet), value));
-        mViewModel.getClimbSpeed().observe(getViewLifecycleOwner(), value -> mHolder.climbSpeed.setValue(value));
+        mViewModel.getClimbSpeed().observe(getViewLifecycleOwner(), value -> mHolder.climbSpeed.setText(String.format(getString(R.string.format_distance_in_feet), value)));
+        mHolder.incrementClimbSpeed.setOnClickListener(v -> mViewModel.incrementClimbSpeed());
+        mHolder.decrementBurrowSpeed.setOnClickListener(v -> mViewModel.decrementClimbSpeed());
 
-        mHolder.flySpeed.setOnValueChangeListener((newValue, oldValue) -> mViewModel.setFlySpeed(newValue));
-        mHolder.flySpeed.setOnFormatValueCallback(value -> String.format(getString(R.string.format_distance_in_feet), value));
-        mViewModel.getFlySpeed().observe(getViewLifecycleOwner(), value -> mHolder.flySpeed.setValue(value));
+        mViewModel.getFlySpeed().observe(getViewLifecycleOwner(), value -> mHolder.flySpeed.setText(String.format(getString(R.string.format_distance_in_feet), value)));
+        mHolder.incrementFlySpeed.setOnClickListener(v -> mViewModel.incrementFlySpeed());
+        mHolder.decrementBurrowSpeed.setOnClickListener(v -> mViewModel.decrementFlySpeed());
 
-        mHolder.swimSpeed.setOnValueChangeListener((newValue, oldValue) -> mViewModel.setSwimSpeed(newValue));
-        mHolder.swimSpeed.setOnFormatValueCallback(value -> String.format(getString(R.string.format_distance_in_feet), value));
-        mViewModel.getSwimSpeed().observe(getViewLifecycleOwner(), value -> mHolder.swimSpeed.setValue(value));
+        mViewModel.getSwimSpeed().observe(getViewLifecycleOwner(), value -> mHolder.swimSpeed.setText(String.format(getString(R.string.format_distance_in_feet), value)));
+        mHolder.incrementSwimSpeed.setOnClickListener(v -> mViewModel.incrementSwimSpeed());
+        mHolder.decrementBurrowSpeed.setOnClickListener(v -> mViewModel.decrementSwimSpeed());
 
         mViewModel.getCanHover().observe(getViewLifecycleOwner(), value -> mHolder.canHover.setChecked(value));
         mHolder.canHover.setOnCheckedChangeListener((buttonView, isChecked) -> mViewModel.setCanHover(isChecked));
@@ -66,21 +73,41 @@ public class EditSpeedFragment extends MCFragment {
 
     private static class ViewHolder {
 
-        final Stepper baseSpeed;
-        final Stepper burrowSpeed;
-        final Stepper climbSpeed;
-        final Stepper flySpeed;
-        final Stepper swimSpeed;
+        final TextView baseSpeed;
+        final Button incrementBaseSpeed;
+        final Button decrementBaseSpeed;
+        final TextView burrowSpeed;
+        final Button incrementBurrowSpeed;
+        final Button decrementBurrowSpeed;
+        final TextView climbSpeed;
+        final Button incrementClimbSpeed;
+        final Button decrementClimbSpeed;
+        final TextView flySpeed;
+        final Button incrementFlySpeed;
+        final Button decrementFlySpeed;
+        final TextView swimSpeed;
+        final Button incrementSwimSpeed;
+        final Button decrementSwimSpeed;
         final SwitchCompat canHover;
         final SwitchCompat hasCustomSpeed;
         final EditText customSpeed;
 
-        ViewHolder(@NonNull View root) {
+        ViewHolder(View root) {
             baseSpeed = root.findViewById(R.id.baseSpeed);
+            incrementBaseSpeed = root.findViewById(R.id.baseSpeed_increment);
+            decrementBaseSpeed = root.findViewById(R.id.baseSpeed_decrement);
             burrowSpeed = root.findViewById(R.id.burrowSpeed);
+            incrementBurrowSpeed = root.findViewById(R.id.burrowSpeed_increment);
+            decrementBurrowSpeed = root.findViewById(R.id.burrowSpeed_decrement);
             climbSpeed = root.findViewById(R.id.climbSpeed);
+            incrementClimbSpeed = root.findViewById(R.id.climbSpeed_increment);
+            decrementClimbSpeed = root.findViewById(R.id.climbSpeed_decrement);
             flySpeed = root.findViewById(R.id.flySpeed);
+            incrementFlySpeed = root.findViewById(R.id.flySpeed_increment);
+            decrementFlySpeed = root.findViewById(R.id.flySpeed_decrement);
             swimSpeed = root.findViewById(R.id.swimSpeed);
+            incrementSwimSpeed = root.findViewById(R.id.swimSpeed_increment);
+            decrementSwimSpeed = root.findViewById(R.id.swimSpeed_decrement);
             canHover = root.findViewById(R.id.canHover);
             hasCustomSpeed = root.findViewById(R.id.hasCustomSpeed);
             customSpeed = root.findViewById(R.id.customSpeed);
