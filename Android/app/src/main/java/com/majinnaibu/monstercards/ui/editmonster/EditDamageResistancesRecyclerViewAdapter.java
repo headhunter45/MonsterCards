7eae6f820edcb5636b14c424dc7721c470212c79
Tom Hicks <headhunter3@gmail.com>
2021-06-20 19:35:16 -0700

    Adds damage resistances to monster editor.

---
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditDamageResistancesRecyclerViewAdapter.java
@@ -0,0 +1,69 @@
+package com.majinnaibu.monstercards.ui.editmonster;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.majinnaibu.monstercards.databinding.FragmentEditDamageResistancesListItemBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * x
+ * {@link RecyclerView.Adapter} that can display a {@link String}.
+ */
+public class EditDamageResistancesRecyclerViewAdapter extends RecyclerView.Adapter<EditDamageResistancesRecyclerViewAdapter.ViewHolder> {
+    private final List<String> mValues;
+    private final ItemCallback mOnClick;
+
+    public EditDamageResistancesRecyclerViewAdapter(List<String> items, ItemCallback onClick) {
+        mValues = items;
+        mOnClick = onClick;
+    }
+
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int viewType) {
+        return new ViewHolder(FragmentEditDamageResistancesListItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mContentView.setText(mValues.get(position));
+        holder.itemView.setOnClickListener(v -> {
+            if (mOnClick != null) {
+                mOnClick.onItemCallback(holder.mItem);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public interface ItemCallback {
+        void onItemCallback(String sense);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mContentView;
+        public String mItem;
+
+        public ViewHolder(FragmentEditDamageResistancesListItemBinding binding) {
+            super(binding.getRoot());
+            mContentView = binding.content;
+        }
+
+        @NotNull
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
