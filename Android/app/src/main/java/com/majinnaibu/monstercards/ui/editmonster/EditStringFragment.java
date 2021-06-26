31bed7f4fafa5c447cca0d3f2300c65f0dd8fda2
Tom Hicks <headunter3@gmail.com>
2021-06-25 20:28:25 -0700

    Replaces condition immunities, damage immunities, damage resistances, damage vulnerabilities, and senses with a unified list of strings editor.

--- Android/app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditStringFragment.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/editmonster/EditStringFragment.java
@@ -15,39 +15,41 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.majinnaibu.monstercards.R;
+import com.majinnaibu.monstercards.data.enums.StringType;
 import com.majinnaibu.monstercards.ui.shared.MCFragment;
 import com.majinnaibu.monstercards.utils.Logger;
 import com.majinnaibu.monstercards.utils.TextChangedListener;
 
-public class EditSenseFragment extends MCFragment {
+public class EditStringFragment extends MCFragment {
     private EditMonsterViewModel mEditMonsterViewModel;
     private EditStringViewModel mViewModel;
     private ViewHolder mHolder;
-    private String mOldSense;
+    private String mOldValue;
+    private StringType mStringType;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         mViewModel = new ViewModelProvider(this).get(EditStringViewModel.class);
         if (getArguments() != null) {
-            EditSenseFragmentArgs args = EditSenseFragmentArgs.fromBundle(getArguments());
-            mOldSense = args.getSense();
-            mViewModel.resetValue(mOldSense);
+            EditStringFragmentArgs args = EditStringFragmentArgs.fromBundle(getArguments());
+            mOldValue = args.getValue();
+            mViewModel.setValue(mOldValue);
+            mStringType = args.getStringType();
         } else {
-            Logger.logWTF("EditSenseFragment needs arguments");
-            mOldSense = null;
+            Logger.logWTF("EditStringFragment needs arguments");
+            mOldValue = null;
         }
         super.onCreate(savedInstanceState);
     }
 
+    @Nullable
+    @org.jetbrains.annotations.Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
         NavBackStackEntry backStackEntry = navController.getBackStackEntry(R.id.edit_monster_navigation);
         mEditMonsterViewModel = new ViewModelProvider(backStackEntry).get(EditMonsterViewModel.class);
-
-        View root = inflater.inflate(R.layout.fragment_edit_sense, container, false);
-
+        View root = inflater.inflate(R.layout.fragment_edit_string, container, false);
         mHolder = new ViewHolder(root);
 
         mHolder.description.setText(mViewModel.getValueAsString());
@@ -57,7 +59,7 @@ public class EditSenseFragment extends MCFragment {
             @Override
             public void handleOnBackPressed() {
                 if (mViewModel.hasChanges()) {
-                    mEditMonsterViewModel.replaceSense(mOldSense, mViewModel.getValueAsString());
+                    mEditMonsterViewModel.replaceString(mStringType, mOldValue, mViewModel.getValueAsString());
                 }
                 Navigation.findNavController(requireView()).navigateUp();
             }
@@ -66,11 +68,14 @@ public class EditSenseFragment extends MCFragment {
         return root;
     }
 
+
     private static class ViewHolder {
         EditText description;
+        EditText name;
 
         ViewHolder(View root) {
-            description = root.findViewById(R.id.name);
+            description = root.findViewById(R.id.description);
+            name = root.findViewById(R.id.name);
         }
     }
 }
