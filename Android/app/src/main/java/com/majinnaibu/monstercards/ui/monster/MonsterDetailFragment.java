2f5918b7a210925dcdb24e4ec8f041155df2f4c7
Tom Hicks <headunter3@gmail.com>
2021-06-27 01:31:13 -0700

    Adds other traits to the monster detail fragment. Cleans up the monster detail fragment to work like the other fragments.

--- Android/app/src/main/java/com/majinnaibu/monstercards/ui/monster/MonsterDetailFragment.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/monster/MonsterDetailFragment.java
@@ -13,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -29,6 +30,7 @@ import com.majinnaibu.monstercards.models.Monster;
 import com.majinnaibu.monstercards.ui.shared.MCFragment;
 import com.majinnaibu.monstercards.utils.Logger;
 
+import java.util.List;
 import java.util.UUID;
 
 import io.reactivex.rxjava3.observers.DisposableSingleObserver;
@@ -36,7 +38,7 @@ import io.reactivex.rxjava3.observers.DisposableSingleObserver;
 public class MonsterDetailFragment extends MCFragment {
     private ViewHolder mHolder;
 
-    private MonsterDetailViewModel monsterDetailViewModel;
+    private MonsterDetailViewModel mViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -45,18 +47,13 @@ public class MonsterDetailFragment extends MCFragment {
         assert arguments != null;
         UUID monsterId = UUID.fromString(MonsterDetailFragmentArgs.fromBundle(arguments).getMonsterId());
         setHasOptionsMenu(true);
-
-        monsterDetailViewModel = new ViewModelProvider(this).get(MonsterDetailViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_monster, container, false);
-
-        mHolder = new ViewHolder(root);
-
+        mViewModel = new ViewModelProvider(this).get(MonsterDetailViewModel.class);
         repository.getMonster(monsterId).toObservable()
                 .firstOrError()
                 .subscribe(new DisposableSingleObserver<Monster>() {
                     @Override
                     public void onSuccess(@io.reactivex.rxjava3.annotations.NonNull Monster monster) {
-                        monsterDetailViewModel.setMonster(monster);
+                        mViewModel.setMonster(monster);
                         dispose();
                     }
 
@@ -66,132 +63,93 @@ public class MonsterDetailFragment extends MCFragment {
                         dispose();
                     }
                 });
+        View root = inflater.inflate(R.layout.fragment_monster, container, false);
+        mHolder = new ViewHolder(root);
 
-        monsterDetailViewModel.getName().observe(getViewLifecycleOwner(), mHolder.name::setText);
-        monsterDetailViewModel.getMeta().observe(getViewLifecycleOwner(), mHolder.meta::setText);
-        monsterDetailViewModel.getArmorClass().observe(getViewLifecycleOwner(), armorText -> mHolder.armorClass.setText(Html.fromHtml("<b>Armor Class</b> " + armorText)));
-        monsterDetailViewModel.getHitPoints().observe(getViewLifecycleOwner(), hitPoints -> mHolder.hitPoints.setText(Html.fromHtml("<b>Hit Points</b> " + hitPoints)));
-        monsterDetailViewModel.getSpeed().observe(getViewLifecycleOwner(), speed -> mHolder.speed.setText(Html.fromHtml("<b>Speed</b> " + speed)));
-        monsterDetailViewModel.getStrength().observe(getViewLifecycleOwner(), mHolder.strength::setText);
-        monsterDetailViewModel.getDexterity().observe(getViewLifecycleOwner(), mHolder.dexterity::setText);
-        monsterDetailViewModel.getConstitution().observe(getViewLifecycleOwner(), mHolder.constitution::setText);
-        monsterDetailViewModel.getIntelligence().observe(getViewLifecycleOwner(), mHolder.intelligence::setText);
-        monsterDetailViewModel.getWisdom().observe(getViewLifecycleOwner(), mHolder.wisdom::setText);
-        monsterDetailViewModel.getCharisma().observe(getViewLifecycleOwner(), mHolder.charisma::setText);
-        monsterDetailViewModel.getSavingThrows().observe(getViewLifecycleOwner(), savingThrows -> {
-            if (StringHelper.isNullOrEmpty(savingThrows)) {
-                mHolder.savingThrows.setVisibility(View.GONE);
-            } else {
-                mHolder.savingThrows.setVisibility(View.VISIBLE);
-            }
-            mHolder.savingThrows.setText(Html.fromHtml("<b>Saving Throws</b> " + savingThrows));
-        });
-        monsterDetailViewModel.getSkills().observe(getViewLifecycleOwner(), skills -> {
-            if (StringHelper.isNullOrEmpty(skills)) {
-                mHolder.skills.setVisibility(View.GONE);
-            } else {
-                mHolder.skills.setVisibility(View.VISIBLE);
-            }
-            mHolder.skills.setText(Html.fromHtml("<b>Skills</b> " + skills));
-        });
-        monsterDetailViewModel.getDamageVulnerabilities().observe(getViewLifecycleOwner(), damageType -> {
-            if (StringHelper.isNullOrEmpty(damageType)) {
-                mHolder.damageVulnerabilities.setVisibility(View.GONE);
-            } else {
-                mHolder.damageVulnerabilities.setVisibility(View.VISIBLE);
-            }
-            mHolder.damageVulnerabilities.setText(Html.fromHtml("<b>Damage Vulnerabilities</b> " + damageType));
-        });
-        monsterDetailViewModel.getDamageResistances().observe(getViewLifecycleOwner(), damageType -> {
-            if (StringHelper.isNullOrEmpty(damageType)) {
-                mHolder.damageResistances.setVisibility(View.GONE);
-            } else {
-                mHolder.damageResistances.setVisibility(View.VISIBLE);
-            }
-            mHolder.damageResistances.setText(Html.fromHtml("<b>Damage Resistances</b> " + damageType));
-        });
-        monsterDetailViewModel.getDamageImmunities().observe(getViewLifecycleOwner(), damageType -> {
-            if (StringHelper.isNullOrEmpty(damageType)) {
-                mHolder.damageImmunities.setVisibility(View.GONE);
-            } else {
-                mHolder.damageImmunities.setVisibility(View.VISIBLE);
-            }
-            mHolder.damageImmunities.setText(Html.fromHtml("<b>Damage Immunities</b> " + damageType));
-        });
-        monsterDetailViewModel.getConditionImmunities().observe(getViewLifecycleOwner(), conditionImmunities -> {
-            if (StringHelper.isNullOrEmpty(conditionImmunities)) {
-                mHolder.conditionImmunities.setVisibility(View.GONE);
-            } else {
-                mHolder.conditionImmunities.setVisibility(View.VISIBLE);
-            }
-            mHolder.conditionImmunities.setText(Html.fromHtml("<b>Condition Immunities</b> " + conditionImmunities));
-        });
-        monsterDetailViewModel.getSenses().observe(getViewLifecycleOwner(), senses -> {
-            if (StringHelper.isNullOrEmpty(senses)) {
-                mHolder.senses.setVisibility(View.GONE);
-            } else {
-                mHolder.senses.setVisibility(View.VISIBLE);
-            }
-            mHolder.senses.setText(Html.fromHtml("<b>Senses</b> " + senses));
-        });
-        monsterDetailViewModel.getLanguages().observe(getViewLifecycleOwner(), languages -> {
-            if (StringHelper.isNullOrEmpty(languages)) {
-                mHolder.languages.setVisibility(View.GONE);
-            } else {
-                mHolder.languages.setVisibility(View.VISIBLE);
-            }
-            mHolder.languages.setText(Html.fromHtml("<b>Languages</b> " + languages));
-        });
-        monsterDetailViewModel.getChallenge().observe(getViewLifecycleOwner(), challengeRating -> mHolder.challenge.setText(Html.fromHtml("<b>Challenge</b> " + challengeRating)));
-        monsterDetailViewModel.getAbilities().observe(getViewLifecycleOwner(), abilities -> {
-            Context context = getContext();
-            DisplayMetrics displayMetrics = null;
-            if (context != null) {
-                Resources resources = context.getResources();
-                if (resources != null) {
-                    displayMetrics = resources.getDisplayMetrics();
-                }
-            }
-            mHolder.abilities.removeAllViews();
-            if (abilities != null) {
-                for (String ability : abilities) {
-                    TextView tvAbility = new TextView(context);
-                    // TODO: Handle multiline block quotes specially so they stay multiline.
-                    // TODO: Replace QuoteSpans in the result of fromHtml with something like this https://stackoverflow.com/questions/7717567/how-to-style-blockquotes-in-android-textviews to make them indent as expected
-                    Spanned spannedText = Html.fromHtml(CommonMarkHelper.toHtml(ability));
-                    tvAbility.setText(spannedText);
-                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    layoutParams.topMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, displayMetrics);
-                    tvAbility.setLayoutParams(layoutParams);
-                    mHolder.abilities.addView(tvAbility);
-                }
-            }
-        });
-        monsterDetailViewModel.getActions().observe(getViewLifecycleOwner(), actions -> {
-            Context context = getContext();
-            DisplayMetrics displayMetrics = null;
-            if (context != null) {
-                Resources resources = context.getResources();
-                if (resources != null) {
-                    displayMetrics = resources.getDisplayMetrics();
-                }
-            }
-            mHolder.actions.removeAllViews();
-            if (actions != null) {
-                for (String action : actions) {
-                    TextView tvAction = new TextView(getContext());
-                    tvAction.setText(Html.fromHtml(CommonMarkHelper.toHtml(action)));
-                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    layoutParams.topMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, displayMetrics);
-                    tvAction.setLayoutParams(layoutParams);
-                    mHolder.actions.addView(tvAction);
-                }
-            }
-        });
-
-        // TODO: add lair actions, legendary actions, reactions, and regional actions
+        mViewModel.getName().observe(getViewLifecycleOwner(), mHolder.name::setText);
+        mViewModel.getMeta().observe(getViewLifecycleOwner(), mHolder.meta::setText);
+        mViewModel.getArmorClass().observe(getViewLifecycleOwner(), armorText -> setupLabeledTextView(mHolder.armorClass, armorText, R.string.label_armor_class));
+        mViewModel.getHitPoints().observe(getViewLifecycleOwner(), hitPoints -> setupLabeledTextView(mHolder.hitPoints, hitPoints, R.string.label_hit_points));
+        mViewModel.getSpeed().observe(getViewLifecycleOwner(), speed -> setupLabeledTextView(mHolder.speed, speed, R.string.label_speed));
+        mViewModel.getStrength().observe(getViewLifecycleOwner(), mHolder.strength::setText);
+        mViewModel.getDexterity().observe(getViewLifecycleOwner(), mHolder.dexterity::setText);
+        mViewModel.getConstitution().observe(getViewLifecycleOwner(), mHolder.constitution::setText);
+        mViewModel.getIntelligence().observe(getViewLifecycleOwner(), mHolder.intelligence::setText);
+        mViewModel.getWisdom().observe(getViewLifecycleOwner(), mHolder.wisdom::setText);
+        mViewModel.getCharisma().observe(getViewLifecycleOwner(), mHolder.charisma::setText);
+        mViewModel.getSavingThrows().observe(getViewLifecycleOwner(), savingThrows -> setupOptionalTextView(mHolder.savingThrows, savingThrows, R.string.label_saving_throws));
+        mViewModel.getSkills().observe(getViewLifecycleOwner(), skills -> setupOptionalTextView(mHolder.skills, skills, R.string.label_skills));
+        mViewModel.getDamageVulnerabilities().observe(getViewLifecycleOwner(), damageTypes -> setupOptionalTextView(mHolder.damageVulnerabilities, damageTypes, R.string.label_damage_vulnerabilities));
+        mViewModel.getDamageResistances().observe(getViewLifecycleOwner(), damageTypes -> setupOptionalTextView(mHolder.damageResistances, damageTypes, R.string.label_damage_resistances));
+        mViewModel.getDamageImmunities().observe(getViewLifecycleOwner(), damageTypes -> setupOptionalTextView(mHolder.damageImmunities, damageTypes, R.string.label_damage_immunities));
+        mViewModel.getConditionImmunities().observe(getViewLifecycleOwner(), conditionImmunities -> setupOptionalTextView(mHolder.conditionImmunities, conditionImmunities, R.string.label_condition_immunities));
+        mViewModel.getSenses().observe(getViewLifecycleOwner(), senses -> setupOptionalTextView(mHolder.senses, senses, R.string.label_senses));
+        mViewModel.getLanguages().observe(getViewLifecycleOwner(), languages -> setupOptionalTextView(mHolder.languages, languages, R.string.label_languages));
+        mViewModel.getChallenge().observe(getViewLifecycleOwner(), challengeRating -> setupLabeledTextView(mHolder.challenge, challengeRating, R.string.label_challenge_rating));
+        mViewModel.getAbilities().observe(getViewLifecycleOwner(), abilities -> setupTraitList(mHolder.abilities, abilities));
+        mViewModel.getActions().observe(getViewLifecycleOwner(), actions -> setupTraitList(mHolder.actions, actions, mHolder.actions_label, mHolder.actions_divider));
+        mViewModel.getReactions().observe(getViewLifecycleOwner(), reactions -> setupTraitList(mHolder.reactions, reactions, mHolder.reactions_label, mHolder.reactions_divider));
+        mViewModel.getRegionalEffects().observe(getViewLifecycleOwner(), regionalEffects -> setupTraitList(mHolder.regionalEffects, regionalEffects, mHolder.regionalEffects_label, mHolder.regionalEffects_divider));
+        mViewModel.getLairActions().observe(getViewLifecycleOwner(), lairActions -> setupTraitList(mHolder.lairActions, lairActions, mHolder.lairActions_label, mHolder.lairActions_divider));
+        mViewModel.getLegendaryActions().observe(getViewLifecycleOwner(), legendaryActions -> setupTraitList(mHolder.legendaryActions, legendaryActions, mHolder.legendaryActions_label, mHolder.legendaryActions_divider));
 
         return root;
+    }
+
+    private void setupLabeledTextView(TextView view, String text, int titleId) {
+        String title = getString(titleId);
+        String fullText = String.format("<b>%s</b> %s", title, text);
+        view.setText(Html.fromHtml(fullText));
+    }
+
+    private void setupOptionalTextView(TextView root, String text, int titleId) {
+        String title = getString(titleId);
+        if (StringHelper.isNullOrEmpty(text)) {
+            root.setVisibility(View.GONE);
+        } else {
+            root.setVisibility(View.VISIBLE);
+        }
+        Spanned formatted;
+        if (StringHelper.isNullOrEmpty(title)) {
+            formatted = Html.fromHtml(text);
+        } else {
+            formatted = Html.fromHtml(String.format("<b>%s</b> %s", title, text));
+        }
+        root.setText(formatted);
+    }
+
+    private void setupTraitList(@NonNull LinearLayout root, @NonNull List<String> traits) {
+        setupTraitList(root, traits, null, null);
+    }
+
+    private void setupTraitList(@NonNull LinearLayout root, @NonNull List<String> traits, View label, View divider) {
+        int visibility = traits.size() > 0 ? View.VISIBLE : View.GONE;
+        Context context = getContext();
+        DisplayMetrics displayMetrics = null;
+        if (context != null) {
+            Resources resources = context.getResources();
+            if (resources != null) {
+                displayMetrics = resources.getDisplayMetrics();
+            }
+        }
+        root.removeAllViews();
+        for (String action : traits) {
+            TextView tvAction = new TextView(getContext());
+            // TODO: Handle multiline block quotes specially so they stay multiline.
+            // TODO: Replace QuoteSpans in the result of fromHtml with something like this https://stackoverflow.com/questions/7717567/how-to-style-blockquotes-in-android-textviews to make them indent as expected
+            tvAction.setText(Html.fromHtml(CommonMarkHelper.toHtml(action)));
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.topMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, displayMetrics);
+            tvAction.setLayoutParams(layoutParams);
+            root.addView(tvAction);
+        }
+        root.setVisibility(visibility);
+        if (label != null) {
+            label.setVisibility(visibility);
+        }
+        if (divider != null) {
+            divider.setVisibility(visibility);
+        }
     }
 
     @Override
@@ -203,7 +161,7 @@ public class MonsterDetailFragment extends MCFragment {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.menu_action_edit_monster) {
-            UUID monsterId = monsterDetailViewModel.getId().getValue();
+            UUID monsterId = mViewModel.getId().getValue();
             if (monsterId != null) {
                 NavDirections action = MonsterDetailFragmentDirections.actionNavigationMonsterToEditMonsterFragment(monsterId.toString());
                 Navigation.findNavController(requireView()).navigate(action);
@@ -238,10 +196,20 @@ public class MonsterDetailFragment extends MCFragment {
         final TextView challenge;
         final LinearLayout abilities;
         final LinearLayout actions;
+        final TextView actions_label;
+        final ImageView actions_divider;
         final LinearLayout reactions;
+        final TextView reactions_label;
+        final ImageView reactions_divider;
         final LinearLayout legendaryActions;
+        final TextView legendaryActions_label;
+        final ImageView legendaryActions_divider;
         final LinearLayout lairActions;
+        final TextView lairActions_label;
+        final ImageView lairActions_divider;
         final LinearLayout regionalEffects;
+        final TextView regionalEffects_label;
+        final ImageView regionalEffects_divider;
 
         ViewHolder(View root) {
             name = root.findViewById(R.id.name);
@@ -266,10 +234,20 @@ public class MonsterDetailFragment extends MCFragment {
             challenge = root.findViewById(R.id.challenge);
             abilities = root.findViewById(R.id.abilities);
             actions = root.findViewById(R.id.actions);
+            actions_divider = root.findViewById(R.id.actions_divider);
+            actions_label = root.findViewById(R.id.actions_label);
             reactions = root.findViewById(R.id.reactions);
+            reactions_divider = root.findViewById(R.id.reactions_divider);
+            reactions_label = root.findViewById(R.id.reactions_label);
             legendaryActions = root.findViewById(R.id.legendaryActions);
+            legendaryActions_divider = root.findViewById(R.id.legendaryActions_divider);
+            legendaryActions_label = root.findViewById(R.id.legendaryActions_label);
             lairActions = root.findViewById(R.id.lairActions);
+            lairActions_divider = root.findViewById(R.id.lairActions_divider);
+            lairActions_label = root.findViewById(R.id.lairActions_label);
             regionalEffects = root.findViewById(R.id.regionalEffects);
+            regionalEffects_divider = root.findViewById(R.id.regionalEffects_divider);
+            regionalEffects_label = root.findViewById(R.id.regionalEffects_label);
         }
     }
 }
