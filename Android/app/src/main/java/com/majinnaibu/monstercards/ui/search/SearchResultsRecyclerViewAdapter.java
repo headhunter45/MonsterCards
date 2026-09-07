d6f5077fd56679de54d12b6702fa40f8d43e9996
Tom Hicks <headhunter3@gmail.com>
2026-09-07 15:21:19 -0700

    Adds collections to search and disambiguates results.

--- Android/app/src/main/java/com/majinnaibu/monstercards/ui/search/SearchResultsRecyclerViewAdapter.java
+++ Android/app/src/main/java/com/majinnaibu/monstercards/ui/search/SearchResultsRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.majinnaibu.monstercards.ui.search;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,7 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.majinnaibu.monstercards.R;
 import com.majinnaibu.monstercards.data.MonsterRepository;
+import com.majinnaibu.monstercards.helpers.StringHelper;
+import com.majinnaibu.monstercards.models.Collection;
 import com.majinnaibu.monstercards.models.Monster;
+import com.majinnaibu.monstercards.models.SearchResultItem;
 import com.majinnaibu.monstercards.utils.Logger;
 
 import java.util.ArrayList;
@@ -23,7 +27,7 @@ public class SearchResultsRecyclerViewAdapter extends RecyclerView.Adapter<Searc
     private final MonsterRepository mRepository;
     private final ItemCallback mOnClickHandler;
     private String mSearchText;
-    private List<Monster> mValues;
+    private List<SearchResultItem> mValues;
     private Disposable mSubscriptionHandler;
 
     public SearchResultsRecyclerViewAdapter(MonsterRepository repository,
@@ -42,9 +46,9 @@ public class SearchResultsRecyclerViewAdapter extends RecyclerView.Adapter<Searc
             mSubscriptionHandler.dispose();
         }
         mSearchText = searchText;
-        Flowable<List<Monster>> foundMonsters = mRepository.searchMonsters(mSearchText);
-        mSubscriptionHandler = foundMonsters.subscribe(monsters -> {
-                    mValues = monsters;
+        Flowable<List<SearchResultItem>> resultsFlowable = mRepository.searchAll(mSearchText);
+        mSubscriptionHandler = resultsFlowable.subscribe(results -> {
+                    mValues = results;
                     notifyDataSetChanged();
                 },
                 throwable -> Logger.logError("Error performing search", throwable));
@@ -54,18 +58,52 @@ public class SearchResultsRecyclerViewAdapter extends RecyclerView.Adapter<Searc
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.monster_list_content, parent, false);
+                .inflate(R.layout.search_result_list_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
-        Monster monster = mValues.get(position);
-        holder.mContentView.setText(monster.name);
-        holder.itemView.setTag(monster);
+        SearchResultItem item = mValues.get(position);
+        if (item.type == SearchResultItem.Type.MONSTER && item.monster != null) {
+            Monster monster = item.monster;
+            holder.mTitleView.setText(monster.name);
+
+            StringBuilder sb = new StringBuilder();
+            if (!StringHelper.isNullOrEmpty(monster.alignment)) {
+                sb.append(monster.alignment);
+            }
+            String cr = monster.getChallengeRatingDescription();
+            if (!StringHelper.isNullOrEmpty(cr)) {
+                if (sb.length() > 0) {
+                    sb.append(", ");
+                }
+                sb.append("CR ").append(cr);
+            }
+            if (sb.length() > 0) {
+                holder.mSubtitleView.setText(sb.toString());
+                holder.mSubtitleView.setVisibility(View.VISIBLE);
+            } else {
+                holder.mSubtitleView.setVisibility(View.GONE);
+            }
+            holder.mIconView.setVisibility(View.GONE);
+        } else if (item.type == SearchResultItem.Type.COLLECTION && item.collection != null) {
+            Collection collection = item.collection;
+            holder.mTitleView.setText(collection.name);
+
+            if (!StringHelper.isNullOrEmpty(collection.description)) {
+                holder.mSubtitleView.setText(collection.description);
+                holder.mSubtitleView.setVisibility(View.VISIBLE);
+            } else {
+                holder.mSubtitleView.setVisibility(View.GONE);
+            }
+            holder.mIconView.setVisibility(View.VISIBLE);
+        }
+
+        holder.itemView.setTag(item);
         holder.itemView.setOnClickListener(view -> {
             if (mOnClickHandler != null) {
-                mOnClickHandler.onItem(monster);
+                mOnClickHandler.onItem(item);
             }
         });
     }
@@ -76,15 +114,19 @@ public class SearchResultsRecyclerViewAdapter extends RecyclerView.Adapter<Searc
     }
 
     public interface ItemCallback {
-        void onItem(Monster monster);
+        void onItem(SearchResultItem item);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        final TextView mContentView;
+        final TextView mTitleView;
+        final TextView mSubtitleView;
+        final ImageView mIconView;
 
         ViewHolder(View view) {
             super(view);
-            mContentView = view.findViewById(R.id.content);
+            mTitleView = view.findViewById(R.id.title);
+            mSubtitleView = view.findViewById(R.id.subtitle);
+            mIconView = view.findViewById(R.id.icon);
         }
     }
 }
